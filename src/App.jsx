3b54247e230d2bdd62ee23de7d6907b56f93b6f9
tofiqueadam <tofiqueadam@gmail.com>
2025-11-17 10:37:30 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import { useState } from "react";
-
-
-
-function App() {
-    const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
+export default function Board() {
   return (
-    <div>
-      <h1>hello</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />    </div>
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
   );
-}
-
-export default App
-
-function MyButton({ count, onClick }) {
-  // Receives shared state and handler from App
-  return <button onClick={onClick}>clicked {count} times</button>;
 }
